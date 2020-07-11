7c126d3c45d0a3619de88705ff0a6e26e2a4bf0b
Peter Elst <peter.elst@gmail.com>
2020-07-11 05:32:38 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# photoframe
+# Raspberry Pi / Inky wHAT digital photo frame
 Digital photo frame using a Raspberry Pi and Inky wHAT e-ink display
---
+++ photoframe.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import argparse
+from PIL import Image
+from inky import InkyWHAT
+import requests
+
+# Keyword for random photo to search
+keyword = 'cat'
+image_url = 'https://source.unsplash.com/400x300/?{0}'.format(keyword)
+# Filename for downloaded image
+img_file = 'photo.jpg'
+
+
+def main():
+    # Initialize InkyWHAT display
+    inky_display = InkyWHAT('red')
+
+    # Download image and save locally
+    img_data = requests.get(image_url).content
+    with open(img_file, 'wb') as handler:
+        handler.write(img_data)
+
+    # Open downloaded image
+    img = Image.open(img_file)
+
+    # Dither downloaded image
+    pal_img = Image.new('P', (1, 1))
+    pal_img.putpalette((255, 255, 255, 0, 0, 0, 255, 0, 0) + (0, 0, 0) * 252)
+    img = img.convert('RGB').quantize(palette=pal_img)
+
+    # Display dithered image
+    inky_display.set_image(img)
+    inky_display.show()
+
+
+if __name__ == '__main__':
+  main()
